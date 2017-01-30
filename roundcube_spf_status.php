0ab41b3b7cce34f4a8d5a4a10214aa39dcc153ba
Nathan Osman <nathan@quickmediasolutions.com>
2017-01-30 13:16:52 -0800

    Initial implementation.

--- roundcube_spf_status.php
+++ roundcube_spf_status.php
@@ -17,23 +17,43 @@ class roundcube_spf_status extends rcube_plugin
 
     function init()
     {
+        $rcmail = rcmail::get_instance();
         if ($rcmail->action == 'show' || $rcmail->action == 'preview') {
-            $this->add_hook('storage_init', array($this, 'fetch_spf_header'));
-            $this->add_hook('message_headers_output', array($this, 'display_spf_header'));
+            $this->add_hook('storage_init', array($this, 'storage_init'));
+            $this->add_hook('message_headers_output', array($this, 'message_headers_output'));
+        } else if ($rcmail->action == '') {
+            $this->add_hook('storage_init', array($this, 'storage_init'));
         }
     }
 
-    function fetch_spf_header($p)
+    function storage_init($p)
     {
-        //...
-
+        $rcmail = rcmail::get_instance();
+        $p['fetch_headers'] = trim($p['fetch_headers'] . ' RECEIVED-SPF');
         return $p;
     }
 
-    function display_spf_header($p)
+    function message_headers_output($p)
     {
-        //...
-
+        $rcmail = rcmail::get_instance();
+        $spf = $p['headers']->get('Received-SPF');
+        if (0 === strpos($spf, 'Pass')) {
+            $img = $this->urlbase . 'images/pass.png';
+            $txt = 'Pass (valid sender)';
+        } else if (0 === strpos($spf, 'Fail')) {
+            $img = $this->urlbase . 'images/fail.png';
+            $txt = 'Fail (invalid sender)';
+        } else {
+            $img = $this->urlbase . 'images/unknown.png';
+            $txt = 'Unknown (missing or neutral header)';
+        }
+        $p['output']['spf'] = array(
+            'title' => 'SPF Status',
+            'value' => html::img(array(
+                'src' => $img,
+                'style' => 'vertical-align: bottom; width: 12px;',
+            )) . ' ' . htmlentities($txt),
+        );
         return $p;
     }
 }
